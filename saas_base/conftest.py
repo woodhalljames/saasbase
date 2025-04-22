d86a2aeba1809e1d43d092e38e7b24ef20d59d45
James <woodhalljames@gmail.com>
2025-04-22 16:12:22 -0400

    Subscription Setup

---
+++ saas_base/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from saas_base.users.models import User
+from saas_base.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ saas_base/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from saas_base.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
